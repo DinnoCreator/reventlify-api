62080816be99c1f57dec8e1e430e9c509e84411a
Edidiong Obodom <edijay17@gmail.com>
2023-06-11 23:25:54 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 //dependencies
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const port = 5000;
 
 //requires root
-const authRoute = require('./routes/auth-routes');
-const usersRoute = require('./routes/users-routes');
+const authRoute = require("./routes/auth-routes");
+const usersRoute = require("./routes/users-routes");
 
 const app = express();
-// const corsOptions = {optionsSuccessStatus: 200, Credential:true, origin:process.env.URL,};
+const whitelist = [process.env.URL, "https://api.paystack.co"];
+const corsOptions = {
+  optionsSuccessStatus: 200,
+  Credential: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 //middlewares
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({
-  limit: '50mb'
-}));
-app.use(cors());
-// app.use(cors(corsOptions));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(cors(corsOptions));
 
 //ROUTES
-app.use('/auth', authRoute);
-app.use('/user', usersRoute);
-
+app.use("/auth", authRoute);
+app.use("/user", usersRoute);
 
 app.listen(port, () => {
   console.log(`Server has started on port ${port}`);

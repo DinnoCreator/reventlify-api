3df4674a9189c1ab06be9cfbb722e159ded18a68
Edidiong Obodom <edijay17@gmail.com>
2023-06-11 23:10:55 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ const authRoute = require('./routes/auth-routes');
 const usersRoute = require('./routes/users-routes');
 
 const app = express();
-const corsOptions = {optionsSuccessStatus: 200, Credential:true, origin:"*",};
+// const corsOptions = {optionsSuccessStatus: 200, Credential:true, origin:process.env.URL,};
 
 //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({
   limit: '50mb'
 }));
-app.use(cors(corsOptions));
+app.use(cors());
+// app.use(cors(corsOptions));
 
 //ROUTES
 app.use('/auth', authRoute);

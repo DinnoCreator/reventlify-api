b69d9630de1665ee3904ab0a3d520874c538140b
Edidiong Obodom <edijay17@gmail.com>
2023-06-11 19:00:19 +0100

    modified:   db.js

--- db.js
+++ db.js
@@ -14,8 +14,9 @@ const pool = new Pool({
   // user: "postgres",
   password: process.env.DB_PASSWORD,
   host: "dpg-ci30ahrhp8u1a1d74bog-a",
-  // host: "dpg-cgu6r3t269vbmepe0b60-a.oregon-postgres.render.com",
+  // host: "dpg-ci30ahrhp8u1a1d74bog-a.oregon-postgres.render.com",
   port: 5432,
+  database: "myapp_4hvr",
   // database: "reventlify",
   ssl: true,
 });

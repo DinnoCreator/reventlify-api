00e57777a979e486425750975e1a2177dacd7aeb
Edidiong Obodom <edijay17@gmail.com>
2023-06-13 13:01:26 +0100

    modified:   controllers/user/services/tickets/ticketsQuery.js

--- controllers/user/services/tickets/ticketsQuery.js
+++ controllers/user/services/tickets/ticketsQuery.js
@@ -7,6 +7,8 @@ exports.ticketQuery = async (req, res) => {
       "SELECT * FROM tickets WHERE ticket_owner_id = $1 ORDER BY (c_date, c_time) DESC",
       [userId]
     );
+     if (ticketsOwnedByClient.rows.length === 0)
+    return res.status(202).json("nothing to show");
     return res.status(200).json(ticketsOwnedByClient.rows[0]);
   } catch (error) {
     return;

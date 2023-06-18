82877e880085a495f38141e75e4e6106a7681359
Edidiong Obodom <edijay17@gmail.com>
2023-06-19 00:50:26 +0100

    modified:   controllers/user/services/events/events_query/sEventQuery.js

--- controllers/user/services/events/events_query/sEventQuery.js
+++ controllers/user/services/events/events_query/sEventQuery.js
@@ -3,16 +3,17 @@ const pool = require("../../../../../db");
 exports.searchEvents = async (req, res) => {
   const { searchCharacters } = req.body;
   try {
+    if (searchCharacters === "") res.status(400).json("Empty search input");
     const searchResult = await pool.query(
       `
-        SELECT regime_name, regime_id
+        SELECT regime_media, regime_name, regime_id
         FROM regimes
         WHERE regime_name like $1 or regime_id like $2
         `,
-      [`${searchCharacters}%`, `${searchCharacters}%`]
+      [`${searchCharacters.toLowerCase()}%`, `${searchCharacters.toLowerCase()}%`]
     );
     if (searchResult.rows.length === 0)
-      return res.status(202).json("no result found");
+      return res.status(202).json("No result found");
     return res.status(200).json(searchResult.rows);
   } catch (error) {
     return res.status(500).json(error.message);

c69b66b523be66ad141c49d4211b6cdce515dec3
Edidiong Obodom <edijay17@gmail.com>
2023-07-09 19:40:42 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const authRoute = require("./routes/auth-routes");
 const usersRoute = require("./routes/users-routes");
 
 const app = express();
-const whitelist = [process.env.URL, "https://api.paystack.co"];
+const whitelist = [process.env.URL, "https://api.paystack.co", "https://paystack.com", "https://paystack.co"];
 const corsOptions = {
   optionsSuccessStatus: 200,
   Credential: true,
